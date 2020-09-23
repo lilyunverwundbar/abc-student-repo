96839b9f3e289f3971786383d5ab7febf3669581
lilyunverwundbar <lilyunverwundbar@gmail.com>
2020-09-23 23:07:11 +0800

    edit the gallery and mini project 1

---
+++ lily-room/room.js
@@ -0,0 +1,17 @@
+function floatAround(obj){
+  let r = Math.random()*50;
+  obj.style.marginLeft=r+'px';
+  let y = Math.random()*300;
+  obj.style.marginTop = y+"px";
+
+  //obj.style.color = 'orange';
+}
+
+function changeColor(obj){
+  obj.style.marginTop = '100px';
+  if (obj.style.color != orange){
+    obj.style.color = orange;
+  }else{
+    obj.style.color = (177,122,125);
+  }
+}
