3edfe23c2e1ba90aa910eb7e2b8d8f3bebcfe76d
lilyunverwundbar <lilyunverwundbar@gmail.com>
2020-10-13 17:17:09 +0800

    adding readme to project A.

---
+++ projects/project-A/README.md
@@ -0,0 +1,12 @@
+#A Cruise with Browser Windows
+![alt text][logo]
+
+[logo]: https://github.com/lilyunverwundbar/abc-student-repo/projects/project-A/demo.gif "demo" <br>
+##Description
+For project A, I used popup windows to make a cruise ship, when you click the anchor, the ship will sail off and make a horn sound. You can also click on the windows on the top two windows and will discover some interesting effect.
+
+##Quick Start
+You can start cruising by clicking [here](https://lilyunverwundbar.github.io/abc-student-repo/projects/project-A/)
+
+##Challenges
+At first I was using css animation to move the sea background image in the popup window and setInterval in javascript in the parent window to move the child windows, yet the movement didn't align together. I was told by professor that maybe I should try to put all the movement in the javascript to make it consistent, and I got a really nice effect as if the ship is really moving on the sea. I also encountered some little problems when the ship is moving backwards, but that was because I messed up with the sign of the left position of the background image, which took relatively less effort.
--- projects/project-A/script.js
+++ projects/project-A/script.js
@@ -1,6 +1,7 @@
 let button = document.getElementById('button');
 let x = 0;
 let y = 0;
+let x2 = -840;
 console.log(screen.width);
 
 
@@ -8,7 +9,7 @@ console.log(screen.width);
 button.addEventListener("click",()=>{
   let topWin1 = window.open("ship/top1.html","","width=300, height=100, left=50,top=0");
   let topWin2 = window.open("ship/top2.html","","width=400, height=100, left=50, top=173" );
-  let newWindow = window.open("ship/ship-bottom.html","","width=600, height=250,left=0, top=323");
+  let newWindow = window.open("ship/ship-bottom.html","","width=600, height=290,left=0, top=323");
   let horn = document.getElementById("horn");
 
 
@@ -39,13 +40,16 @@ button.addEventListener("click",()=>{
             topWin1.moveBy(0,2);
             topWin2.moveBy(0,2);
             newWindow.moveBy(0,2);
-            newWindow.document.getElementById('sea').style.transform = "translate(0px," +(-1*y) + "px";
+            newWindow.document.getElementById('sea').style.transform = "translate(-840px," +(-1*y) + "px";
           }else if(x>0&&y>0){
             x-=2;
+
+            x2+=2;
+
             topWin1.moveBy(-2,0);
             topWin2.moveBy(-2,0);
             newWindow.moveBy(-2,0);
-            newWindow.document.getElementById('sea').style.transform = "translate(" +x + "px,0px";
+            newWindow.document.getElementById('sea').style.transform = "translate(" +x2 + "px,"+(-1*y)+"px";
 
 
           }
