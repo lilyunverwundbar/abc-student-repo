5ec572e9f868d3361ee7aafc56afa7a48c67e1d8
lilyunverwundbar <lilyunverwundbar@gmail.com>
2020-11-25 15:25:22 +0800

    mini 7

---
+++ projects/mini-project-7/socket/public/script.js
@@ -0,0 +1,65 @@
+let socket = io();
+
+let chatbox = document.getElementById("chat");
+let messagebox = document.getElementById("message");
+let sendbutton = document.getElementById("send");
+
+
+sendbutton.addEventListener("click",()=>{
+  console.log("clicked!");
+  let message = messagebox.value.trim();
+  let move = false;
+  if(message != ""){
+    if(message == "Hi"|| message == "hi"|| message == "hello"|| message == "Hello"){
+      message = "(*´▽｀)ノノ😺";
+      move = true;
+    }
+    let data = {message:message, move:move};
+    socket.emit("message",data);
+    console.log(data);
+
+  }
+
+  messagebox.value ="";
+
+
+})
+
+let colors = ["red","orange","blue","cyan","yellow","green"];
+
+socket.on("incoming",(data)=>{
+  console.log(data);
+  let message = data.message;
+  let move = data.move;
+  let li = document.createElement("li");
+  li.style.position = "absolute";
+  let p = document.createElement("p");
+  let angle = 0;
+  let left = 0;
+  if (move==true){
+    setInterval(()=>{
+      console.log(angle);
+      angle += 30;
+      left += 10;
+      li.style.transform = "rotate("+angle+"deg)";
+      li.style.marginLeft = left + "px";
+      p.style.fontSize = "x-large";
+      let top = 0;
+      top += Math.random()*20;
+      li.style.marginTop = top + "px";
+
+    },500);
+
+  }else{
+    li.style.marginTop = Math.random()*300+"px";
+    li.style.marginLeft = Math.random()*100+"px";
+    let idx = Math.floor(Math.random()*6);
+    console.log(idx);
+    p.style.color = colors[idx];
+  }
+
+
+  p.innerHTML = message;
+  li.appendChild(p);
+  chatbox.appendChild(li);
+})
