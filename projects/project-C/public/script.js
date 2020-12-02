fb350f6c7e2fc8310236377d8f59ede2141c7b3e
lilyunverwundbar <lilyunverwundbar@gmail.com>
2020-12-02 21:16:28 +0800

    project c

--- projects/project-C/public/script.js
+++ projects/project-C/public/script.js
@@ -1,55 +1,169 @@
-// let socket = io();
+let socket = io();
+let body = document.getElementsByTagName("body")[0];
+socket.on("hi",function(latterns){
+  console.log(latterns);
+  for(let i = 0 ; i < latterns.length; i++){
+    let con = document.createElement("div");
+    con.style.width = "200px";
+    con.style.height = "200px";
+    //con.style.backgroundColor = "white";
 
+    let d = latterns[i];
+
+
+    let a = d.left;
+    let b = d.top;
+    con.style.left = a;
+    con.style.top = b;
+    con.style.position = "absolute";
+
+
+
+    let newlattern = document.createElement("img");
+    newlattern.src = "lattern.png";
+    newlattern.style.width = "200px";
+    newlattern.style.height = "200px";
+    newlattern.style.left = "0px";
+    newlattern.style.top = "0px";
+    newlattern.style.position = "absolute";
+
+    con.appendChild(newlattern);
+    body.appendChild(con);
+
+  }
+});
+
+
+
+let button = document.getElementById("button");
+let container = document.createElement("div");
+container.style.width = "200px";
+container.style.height = "200px";
 
 let img = document.createElement("img");
-console.log(img);
-img.src = "lattern.png";
-img.style.width = "100px";
-img.style.height = "100px";
-img.style.left = Math.random()*(screen.width - 100) + "px";
-img.style.top = Math.random()*(screen.height - 100) + "px";
-document.getElementById("body").appendChild(img);
+let p = document.createElement("p");
+p.innerHTML = "My lattern";
+p.style.left = "50px";
+p.style.color = "white";
+p.style.position = "absolute";
 
-// socket.emit("message",data);
-// console.log(data);
-//
-//
-//
-// let colors = ["red","orange","hotpink","cyan","yellow","green"];
-//
-// socket.on("incoming",(data)=>{
-//   console.log(data);
-//   let message = data.message;
-//   let move = data.move;
-//   let li = document.createElement("li");
-//   li.style.position = "absolute";
-//   let p = document.createElement("p");
-//   let angle = 0;
-//   let left = 0;
-//   if (move==true){
-//     setInterval(()=>{
-//       console.log(angle);
-//       angle += 30;
-//       left += 10;
-//       li.style.transform = "rotate("+angle+"deg)";
-//       li.style.marginLeft = left + "px";
-//       p.style.fontSize = "x-large";
-//       let top = 0;
-//       top += Math.random()*20;
-//       li.style.marginTop = top + "px";
-//
-//     },100);
-//
-//   }else{
-//     li.style.marginTop = Math.random()*300+"px";
-//     li.style.marginLeft = Math.random()*500+"px";
-//     let idx = Math.floor(Math.random()*6);
-//     console.log(idx);
-//     p.style.color = colors[idx];
-//   }
-//
-//
-//   p.innerHTML = message;
-//   li.appendChild(p);
-//   chatbox.appendChild(li);
-// })
+let data;
+let left = Math.random()*(screen.width/2);
+let t = Math.random()*(screen.height/6);
+let clicked = 0;
+
+
+button.addEventListener("click",()=>{
+  clicked += 1;
+
+
+  img.src = "lattern.png";
+  img.style.width = "200px";
+  img.style.height = "200px";
+  img.style.left = "0px";
+  img.style.top = "0px";
+  img.style.position = "absolute";
+  container.style.left = left + "px";
+  container.style.top = t + "px";
+  //container.style.backgroundColor = "white";
+  container.style.position = "absolute";
+
+
+  data = {left:container.style.left, top: container.style.top, lightsource:undefined, brightness:undefined};
+  console.log(data);
+  if(clicked == 1){
+    console.log("This is my lattern");
+
+    container.appendChild(p);
+
+    container.appendChild(img);
+    socket.emit("message",data);
+
+    body.appendChild(container);
+  };
+
+})
+
+
+let idx = Math.floor(Math.random()*3);
+let img_click = 0;
+let ll = ["light.png","light2.png","light4.png"];
+let light = document.createElement("img");
+light.src = ll[idx];
+light.style.width = "100px";
+light.style.height = "100px";
+light.style.left = "50px";
+light.style.top = "3.5px";
+light.style.position = "absolute";
+light.style.opacity = 0;
+
+
+img.addEventListener("click",()=>{
+
+
+  img_click += 1;
+  light.style.opacity = 1 * (img_click/10);
+
+  console.log("click lattern", img_click);
+
+  data.lightsource = light.src;
+  data.brightness = light.style.opacity;
+  console.log(data);
+
+
+
+  socket.emit("changeLight",data);
+
+
+})
+
+container.appendChild(light);
+
+
+
+
+
+
+
+socket.on("other",function(data){
+  let a = data.left;
+  let b = data.top;
+
+  let con = document.createElement("div");
+  con.style.width = "200px";
+  con.style.height = "200px";
+  con.style.left = a;
+  con.style.top = b;
+  con.style.position = "absolute";
+  //con.style.backgroundColor = "white";
+
+  let newlattern = document.createElement("img");
+  newlattern.src = "lattern.png";
+  newlattern.style.width = "200px";
+  newlattern.style.height = "200px";
+  newlattern.style.left = "0px";
+  newlattern.style.top = "0px";
+  newlattern.style.position = "absolute";
+
+
+//注意不能叠加light的图片因为这样总的brightness不对
+  let light = document.createElement("img");
+  if(data.lightsource){
+
+    light.src = data.lightsource;
+
+    light.style.width = "100px";
+    light.style.height = "100px";
+    light.style.left = "50px";
+    light.style.top = "3.5px";
+    light.style.position = "absolute";
+    light.style.opacity = data.brightness;
+    con.appendChild(light);
+
+  }
+
+  con.appendChild(newlattern);
+  body.appendChild(con);
+
+
+})
--- projects/project-C/server.js
+++ projects/project-C/server.js
@@ -3,6 +3,8 @@ let app = express();
 let http = require('http').createServer(app);
 let io = require('socket.io')(http);
 const port = 3000;
+
+let latterns = [];
 //const port = process.env.PORT;
 
 app.use(express.static('public'));
@@ -13,17 +15,38 @@ app.get('/', (req, res) => {
 io.on('connection', (socket) => {
   console.log('a user connected',socket.id);
 
-  io.emit("add-your-lattern","here is your lattern");
+//when one socket connects, the server send it all the latterns
+  socket.emit("hi",latterns);
 
-  socket.on("disconnect",()=> {
-    console.log("user disconnected", socket.id);
-  });
+  // socket.on("disconnect",()=> {
+  //   console.log("user disconnected", socket.id);
+  // });
 
 //receive from one socket and broadcast to all the sockets
   socket.on("message",(data)=>{
+    latterns.push(data);
     console.log(data);
-    io.emit("incoming",data);
+    socket.broadcast.emit('other',data);
   });
+
+
+//if the server receives info that one lattern's brightness is updated
+  socket.on("changeLight",(data)=>{
+    latterns.forEach(d=>{
+      if (d.left == data.left && d.top == data.top){
+        d.lightsource = data.lightsource;
+        d.brightness = data.brightness;
+      }
+
+      console.log("the updated lattern in the lattern list",d);
+    })
+
+
+    socket.broadcast.emit("other",data);
+
+  });
+
+
 });
 
 http.listen(port, () => {
